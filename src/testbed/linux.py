e44fa4fea14e5496ff14bc02ebe253304ac4dee5
Russell Keith-Magee <russell@keith-magee.com>
2022-06-10 14:50:52 +0800

    Add GTK ecosystem and Pillow tests on Linux.

--- src/testbed/linux.py
+++ src/testbed/linux.py
@@ -1,7 +1,7 @@
 ###########################################################################
 # Linux specific tests
 ###########################################################################
-import importlib
+import os
 import sys
 
 from .utils import assert_
@@ -22,3 +22,83 @@ def test_dbm_gdbm():
 
         assert_(db['hello'] == b'world')
 
+
+def test_glib():
+    "Glib is available"
+    import gi
+    gi.require_version('GLib', '2.0')
+    from gi.repository import GLib
+
+    assert GLib.base64_encode(b"hello world") == "aGVsbG8gd29ybGQ="
+
+
+def test_pango():
+    "Pango is available"
+    import gi
+    gi.require_version('Pango', '1.0')
+    from gi.repository import Pango
+
+    font = Pango.FontDescription()
+    font.set_family("Cantarell")
+    font.set_size(16 * Pango.SCALE)
+    font.set_weight(Pango.Weight.BOLD)
+    assert_(font.to_string() == "Cantarell Bold 16")
+
+
+def test_gdk():
+    "GDK is available"
+    import gi
+    gi.require_version('Gdk', '3.0')
+    from gi.repository import Gdk
+
+    color = Gdk.Color(37, 42, 69)
+    assert_(color.to_string() == "#0025002a0045")
+
+
+def test_gtk():
+    "GTK is available"
+    import gi
+    gi.require_version('Gtk', '3.0')
+    from gi.repository import Gtk
+
+    app = Gtk.Application()
+    assert_(app.get_windows() == [])
+
+
+def test_gdkpixbuf():
+    "GDKPixbuf is available"
+    import gi
+    gi.require_version('GdkPixbuf', '2.0')
+    from gi.repository import GdkPixbuf
+
+    pixbuf = GdkPixbuf.Pixbuf.new_from_file(
+        os.path.join(
+            os.path.dirname(__file__),
+            "resources",
+            "test-pattern.png"
+        )
+    )
+    assert_(pixbuf.get_height() == 768)
+
+
+def test_cairo():
+    "Cairo is available"
+    import cairo
+
+    surface = cairo.ImageSurface(cairo.Format.ARGB32, 100, 200)
+    assert_(surface.get_width() == 100)
+
+
+def test_pillow():
+    "Pillow can be used to load images"
+    from PIL import Image
+
+    for extension in ['png', 'jpg']:
+        image = Image.open(
+            os.path.join(
+                os.path.dirname(__file__),
+                "resources",
+                f"test-pattern.{extension}"
+            )
+        )
+        assert_(image.size == (1366, 768))

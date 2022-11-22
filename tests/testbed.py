61ee42f2c9c82d8b6801bbb7f35d666a710f348d
Russell Keith-Magee <russell@keith-magee.com>
2022-11-22 15:44:32 +0800

    Unpack Android sources on test startup.

--- tests/testbed.py
+++ tests/testbed.py
@@ -1,11 +1,34 @@
 import os
+import sys
 import tempfile
 from pathlib import Path
 
 import pytest
 
+import tests
+
+
+def chaquopy_extract_package(pkg):
+    finder = pkg.__loader__.finder
+    for path in pkg.__path__:
+        chaquopy_extract_dir(finder, finder.zip_path(path))
+
+
+def chaquopy_extract_dir(finder, zip_dir):
+    for filename in finder.listdir(zip_dir):
+        zip_path = f"{zip_dir}/{filename}"
+        if finder.isdir(zip_path):
+            chaquopy_extract_dir(finder, zip_path)
+        else:
+            finder.extract_if_changed(zip_path)
+
+
 if __name__ == "__main__":
     os.chdir(Path(__file__).parent.parent)
+
+    if hasattr(sys, "getandroidapilevel"):
+        chaquopy_extract_package(tests)
+
     result = pytest.main(
         [
             # Turn up verbosity
@@ -15,7 +38,6 @@ if __name__ == "__main__":
             # Override the cache directory to be somewhere known writable
             "-o",
             f"cache_dir={tempfile.gettempdir()}/.pytest_cache",
-            # Run only the test folder
-            "tests",
         ]
+        + tests.__path__
     )

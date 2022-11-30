e99738beb3bdd48294dc02ac26866338f89454e5
Russell Keith-Magee <russell@keith-magee.com>
2022-11-30 12:38:59 +0800

    Update project to match recent briefcase-template changes.

--- tests/testbed.py
+++ tests/testbed.py
@@ -5,39 +5,47 @@ from pathlib import Path
 
 import pytest
 
-import tests
 
-
-def chaquopy_extract_package(pkg):
-    finder = pkg.__loader__.finder
-    for path in pkg.__path__:
-        chaquopy_extract_dir(finder, finder.zip_path(path))
-
-
-def chaquopy_extract_dir(finder, zip_dir):
-    for filename in finder.listdir(zip_dir):
-        zip_path = f"{zip_dir}/{filename}"
-        if finder.isdir(zip_path):
-            chaquopy_extract_dir(finder, zip_path)
-        else:
-            finder.extract_if_changed(zip_path)
-
-
-if __name__ == "__main__":
+def run_tests():
+    project_path = Path(__file__).parent.parent
     os.chdir(Path(__file__).parent.parent)
 
+    ##################################################################
+    # WORKAROUND - On Android, we need to explicitly unpack the test
+    # source code into a location where it can be discovered.
+    ##################################################################
     if hasattr(sys, "getandroidapilevel"):
-        chaquopy_extract_package(tests)
+        import tests
 
-    result = pytest.main(
+        def chaquopy_extract_package(pkg):
+            finder = pkg.__loader__.finder
+            for path in pkg.__path__:
+                chaquopy_extract_dir(finder, finder.zip_path(path))
+
+        def chaquopy_extract_dir(finder, zip_dir):
+            for filename in finder.listdir(zip_dir):
+                zip_path = f"{zip_dir}/{filename}"
+                if finder.isdir(zip_path):
+                    chaquopy_extract_dir(finder, zip_path)
+                else:
+                    finder.extract_if_changed(zip_path)
+
+        chaquopy_extract_package(tests)
+    ##################################################################
+
+    pytest.main(
         [
             # Turn up verbosity
             "-vv",
             # Disable color
             "--color=no",
-            # Override the cache directory to be somewhere known writable
+            # Overwrite the cache directory to somewhere writable
             "-o",
             f"cache_dir={tempfile.gettempdir()}/.pytest_cache",
+            project_path / "tests",
         ]
-        + tests.__path__
     )
+
+
+if __name__ == "__main__":
+    run_tests()

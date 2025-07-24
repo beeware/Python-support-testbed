feca4325262e68a99078d8a65e4adf3c5e1aa975
Russell Keith-Magee <russell@keith-magee.com>
2025-07-24 08:15:38 +0800

    Apply changes suggested by ruff.

--- pth-tester/pth_tester.py
+++ pth-tester/pth_tester.py
@@ -2,7 +2,7 @@ initialized = False
 has_socket = False
 
 
-# The pth_tester module should be initalized by processing the `.pth` file
+# The pth_tester module should be initialized by processing the `.pth` file
 # created on installation.
 def init():
     global initialized
